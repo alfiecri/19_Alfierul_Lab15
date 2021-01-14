e94926de20b8af683f83095cf3b306c1cbbd984f
alfiecri <jediknightalfie@gmail.com>
2021-01-14 15:03:23 +0800

    Initial commit

---
+++ Assets/Scripts/BGM.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGM : MonoBehaviour
+{
+    public AudioClip[] BGMArr;
+    private AudioSource audioSource;
+
+    float volSlider = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            audioSource.Stop();
+            int rand = Random.Range(0, BGMArr.Length);
+            audioSource.PlayOneShot(BGMArr[rand]);
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            audioSource.volume = audioSource.volume + Time.deltaTime + volSlider;
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            audioSource.volume = audioSource.volume + Time.deltaTime - volSlider;
+        }
+    }
+}
---
+++ Assets/Scripts/CharacterScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterScript : MonoBehaviour
+{
+    public GameObject jumpText;
+
+    private int JumpCounter;
+    public AudioClip[] audioClipArr;
+    private AudioSource audioSource;
+    private Rigidbody rb;
+
+    int jumpSpeed = 6;
+    bool isOnFloor = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnFloor == true)
+        {
+            isOnFloor = false;
+            JumpCounter++;
+            jumpText.GetComponent<Text>().text = "Jump: " + JumpCounter;
+            rb.AddForce(Vector3.up * jumpSpeed, ForceMode.Impulse);
+
+            // audioSource.Play();
+
+            int rand = Random.Range(0, audioClipArr.Length);
+            audioSource.PlayOneShot(audioClipArr[rand]);
+        }
+       
+    }
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Floor"))
+        {
+            isOnFloor = true;
+        }
+    }
+}
